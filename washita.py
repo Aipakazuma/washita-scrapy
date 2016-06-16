c97ba2c2fcea272e101f06e9c782645da2676f75
玉城一磨 <kazuma.tamashiro@azana.co.jp>
2016-06-16 18:44:55 +0900

    [add] version up.

--- washita.py
+++ washita.py
@@ -20,42 +20,57 @@ def get_web_esource(url):
         return None
 
 
-def get_item_scrapy(url):
+def get_item_scrapy(url, f):
     resource = get_web_esource(url)
     read_resource = resource.read()
     bs_obj = BeautifulSoup(read_resource, 'lxml')
 
-    # product codeの取得
-    product_code = bs_obj.find('span', {'id': 'product_code_default'}).getText().strip()
+    try:
+        # product codeの取得
+        product_code = bs_obj.find('span', {'id': 'product_code_default'}).getText().strip()
 
-    # カテゴリの取得
-    pankuzu_list = bs_obj.find('ul', {'id': 'topicpath'}).findAll('li')
-    category_name = pankuzu_list[1].getText().strip()
+        # カテゴリの取得
+        pankuzu_list = bs_obj.find('ul', {'id': 'topicpath'}).findAll('li')
+        category_name = pankuzu_list[1].getText().strip()
 
-    # カテゴリリストの取得
-    category_sum_name = pankuzu_list[2].getText().strip()
+        # カテゴリリストの取得
+        category_sum_name = pankuzu_list[2].getText().strip()
 
-    print(product_code + ',' + category_name + ',' + category_sum_name)
+        # 画像を取得
+        image_src = bs_obj.find('img', {'class': 'picture'})['src']
+
+        # 説明を取得
+        explain_text = bs_obj.find('div', {'class': 'main_comment'}).getText().strip().replace('\n', '').replace('\r', '')
+
+        f.write(product_code + ',' +
+                category_name + ',' +
+                category_sum_name + ',' +
+                image_src + ',"' +
+                explain_text + '"\n')
+    except IndexError as e:
+        print(e)
+        print(url)
 
 
-def get_items_scrapy(items):
+def get_items_scrapy(items, f):
     for item in items:
         a = item.find('h3').find('a')
-        get_item_scrapy(site_url + a['href'])
+        get_item_scrapy(site_url + a['href'], f)
 
 
 def main():
     # argv = sys.argv
-    url = 'http://www.washita.co.jp/products/list.php?transactionid=a444640e487c0b51ad18c9a282be125f8a6259d7&mode=search&category_id=0&maker_id=0&name=&product_code=&product_jancode=&price02_min=&price02_max=&orderby=&disp_number=200&product_id=&classcategory_id1=&classcategory_id2=&product_class_id=&quantity=&rnd=mor'
-    # parameter_array = ['pri', 'tea', 'sti', 'pai', 'rac', 'cla', 'dri', 'phi', 'wae', 'nou']
-    # parameter_array = ['pri']
+    url = 'http://www.washita.co.jp/products/list.php?transactionid=824190a3872b5976383c102d4a2b1d11fb06c9bb&mode=search&category_id=0&maker_id=0&name=&product_code=&product_jancode=&price02_min=&price02_max=&orderby=&disp_number=200&product_id=&classcategory_id1=&classcategory_id2=&product_class_id=&quantity=&rnd=mou'
     parameter = '&pageno='
-    for i in range(6, 11):
+    f = open('result_20160616.csv', 'w')
+    for i in range(1, 11):
         resource = get_web_esource(url + parameter + str(i))
         read_resource = resource.read()
         bs_obj = BeautifulSoup(read_resource, 'lxml')
         items = bs_obj.findAll('li', {'class': 'product_item'})
-        get_items_scrapy(items)
+        get_items_scrapy(items, f)
+        print(i)
+    f.close()
 
 
 if __name__ == '__main__':
